8800554eb9baa3d07b03b5982b84b786a8bc9b3b
Leask Wong <i@leaskh.com>
2021-05-11 19:41:15 -0400

    adding geoIp feature

--- controllers/apiSubconscious.js
+++ controllers/apiSubconscious.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { utilitas } = require('utilitas');
+const { utilitas, geoIp } = require('utilitas');
 
 const httpStatus = require('http-status');
 const path = require('path');
@@ -9,14 +9,16 @@ const fs = require('fs').promises;
 const [ptcHttp, ptcHttps] = ['http', 'https'];
 const [wildcardPath, wildcardMethod] = [['*'], ['*']];
 
-const analyze = (ctx, next) => {
+const analyze = async (ctx, next) => {
     ctx.originProtocol = ctx.socket.encrypted || (ctx.app.proxy
         && ctx.get('X-Forwarded-Proto').split(/\s*,\s*/)[0] === ptcHttps)
         ? ptcHttps : ptcHttp; // patch: https://github.com/koajs/koa/issues/974
     ctx.cookies.secure = ctx.encrypted = ctx.originProtocol === ptcHttps;
     ctx.userAgent._agent.versionNormalized
         = utilitas.parseVersion(ctx.userAgent.version);
-    return next();
+    ctx.userAgent._agent.geoIp
+        = ctx.request.ip ? await geoIp.lookup(ctx.request.ip) : null;
+    await next();
 };
 
 const extendCtx = async (ctx, next) => {
