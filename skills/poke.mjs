42b5040a3d70fbf08cbb2df2bdb645c6b8c7e7e7
Leask Wong <i@leaskh.com>
2023-03-27 13:38:17 -0400

    upgraded poke skills

--- skills/poke.mjs
+++ skills/poke.mjs
@@ -1,10 +1,9 @@
 import { checkLink } from '../lib/robot.mjs';
 
-const action = async (bot) => {
-    bot.hears('poke', async (ctx, next) => {
-        ctx.reply(new Date());
-        await next();
-    });
+const action = async (ctx, next) => {
+    if (ctx.end) { return await next(); }
+    ctx.text === 'poke' && ctx.reply(new Date());
+    await next();
 };
 
 export const { run, priority, func } = {
