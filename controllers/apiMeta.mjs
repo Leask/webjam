00253335b9e5583fc1681bd0b64e226476b2d73b
Leask Wong <i@leaskh.com>
2024-01-26 05:48:05 -0500

    enabled vector meta searching

--- controllers/apiMeta.mjs
+++ controllers/apiMeta.mjs
@@ -1,80 +1,35 @@
-import { dbio, meta } from '../index.mjs';
-
-const [MAX_LIMIT, DEFAULT_LIMIT] = [1000, 10];
+import { meta } from '../index.mjs';
 
 const updateById = async (ctx, next) => {
-    try {
-        ctx.ok(await meta._.updateById(ctx.params.class, ctx.params.id, {
-            ...ctx.request.body,
-            updated_by: ctx.verification.user.id,
-        }));
-    } catch (err) { ctx.er(err, 400); }
+    ctx.ok(await meta.updateById(
+        ctx.params.class, ctx.params.id,
+        ctx.request.body, ctx.verification.user.id
+    ));
 };
 
 const create = async (ctx, next) => {
-    try {
-        ctx.ok(await meta._.insert(ctx.params.class, {
-            ...ctx.request.body,
-            created_by: ctx.verification.user.id,
-            updated_by: ctx.verification.user.id,
-        }));
-    } catch (err) { ctx.er(err, 400); }
+    ctx.ok(await meta.insert(
+        ctx.params.class, ctx.request.body, ctx.verification.user.id
+    ));
 };
 
 const getById = async (ctx, next) => {
-    try {
-        const resp = await meta._.queryById(ctx.params.class, ctx.params.id);
-        assert(resp, 'Meta Not Found', 404)
-        ctx.ok(resp);
-    } catch (err) { ctx.er(err, 400); }
+    ctx.ok(await meta.queryById(
+        ctx.params.class, ctx.params.id, ctx.verification.user.id
+    ));
 };
 
 const deleteById = async (ctx, next) => {
-    try {
-        const resp = await meta._.deleteById(ctx.params.class, ctx.params.id);
-        assert(resp?.rowCount, 'Meta Not Found', 404)
-        ctx.ok({});
-    } catch (err) { ctx.er(err, 400); }
+    ctx.ok((await meta.deleteById(
+        ctx.params.class, ctx.params.id, ctx.verification.user.id
+    )) && {});
 };
 
 const query = async (ctx, next) => {
-    try {
-        const table = await meta.assertClass(ctx.params.class);
-        const { order, sort, limit, offset } = {
-            order: ctx.query?.order || 'created_at',
-            sort: ctx.query?.sort || 'ASC',
-            limit: parseInt(ctx.query?.limit) || DEFAULT_LIMIT,
-            offset: parseInt(ctx.query?.offset) || 0,
-        };
-        const whereKey = ` WHERE "created_by" = $1 OR "shared" > $2 `;
-        const whereVal = [ctx.verification.user.id, 0];
-        assert(['created_at', 'updated_at'].includes(order), 'Invalid order.', 400);
-        assert(['ASC', 'DESC'].includes(sort), 'Invalid order.', 400);
-        assert(0 < limit && limit <= MAX_LIMIT, 'Invalid limit.', 400);
-        const [cResp, resp] = await Promise.all([[
-            dbio.assembleQuery(table, { fields: 'COUNT("id")' })
-            + whereKey, whereVal,
-        ], [
-            dbio.assembleQuery(table) + whereKey
-            + dbio.assembleTail({ order: { [order]: sort }, limit, offset }),
-            whereVal,
-        ]].map(x => dbio.query(...x)));
-        assert(resp.length, 'Meta Not Found', 404)
-        const total = parseInt(cResp[0]['count']);
-        ctx.ok({
-            pagination: {
-                total, order, sort, limit, offset,
-                more: offset + resp.length < total,
-            }, metas: resp,
-        });
-    } catch (err) { ctx.er(err, 400); }
+    ctx.ok(await meta.query(
+        ctx.params.class, ctx.verification.user.id, ctx.query
+    ));
 };
-
-/*
-@todo:
-1: only allow creater to update meta or shared > 1
-2: pack meta, not returning vector to frontend
-*/
 
 export const { link, actions } = {
     link: 'file',
