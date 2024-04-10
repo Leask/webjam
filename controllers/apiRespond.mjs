3ff0f421b999a79560f5eba895b96ff48fa38fe5
Leask Wong <i@leaskh.com>
2024-04-10 04:34:52 -0400

    finished respond feature version 1

---
+++ controllers/apiRespond.mjs
@@ -0,0 +1,21 @@
+import { respond } from '../index.mjs';
+
+const act = async (ctx, next) => {
+    ctx.ok(await respond.act(
+        ctx.params.class, ctx.params.id,
+        ctx.request.body?.type, ctx.request.body?.value,
+        ctx.verification.user.id,
+    ));
+};
+
+export const { link, actions } = {
+    link: 'file',
+    actions: [
+        {
+            path: 'api/respond/:class/:id',
+            method: ['POST', 'PUT', 'PATCH'],
+            process: act,
+            auth: true,
+        },
+    ]
+};
--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ export * from 'utilitas';
 export * as file from './lib/file.mjs';
 export * as identity from './lib/identity.mjs';
 export * as meta from './lib/meta.mjs';
+export * as respond from './lib/respond.mjs';
 export * as robot from './lib/robot.mjs';
 export * as router from './lib/router.mjs';
 export * as service from './lib/service.mjs';
